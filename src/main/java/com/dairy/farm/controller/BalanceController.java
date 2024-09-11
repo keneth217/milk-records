730ac539ed8df74914cea3b7d44c7bc066773801
keneth217 <kipyegonkeneth03@gmail.com>
2024-09-11 17:41:28 +0300

    fix payments api,add cors configuration

--- src/main/java/com/dairy/farm/controller/BalanceController.java
+++ src/main/java/com/dairy/farm/controller/BalanceController.java
@@ -4,12 +4,14 @@ import com.dairy.farm.dto.DashboardTotals;
 import com.dairy.farm.service.BalanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/dashboard")
+
 public class BalanceController {
 
 
