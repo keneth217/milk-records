763d8210b00119a459526341d60e986f5131a164
keneth217 <kipyegonkeneth03@gmail.com>
2024-09-16 18:01:48 +0300

    fix dashboard totals,add last and current month comparisons

--- src/main/java/com/dairy/farm/dto/DashboardTotals.java
+++ src/main/java/com/dairy/farm/dto/DashboardTotals.java
@@ -1,15 +1,23 @@
 package com.dairy.farm.dto;
 
+import com.dairy.farm.entity.Payment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class DashboardTotals {
-    private Double totalLitresOfMilk;
+    private Double totalLitres;
     private Double totalAmountPaid;
     private Double totalBalance;
-    private Double totalOverpayments;
+    private Double  totalOverpayments; // This should be List<Payment>
+    private Double lastMonthTotalEarnings;
+    private Double thisMonthTotalEarnings;
+    private Double lastMonthTotalLitres;
+    private Double thisMonthTotalLitres;
+
 }
