1b0b829f9fe966d99ed8b032c7f1d519590e045b
keneth217 <kipyegonkeneth03@gmail.com>
2024-09-17 17:20:35 +0300

    generate pdf apis

--- src/main/java/com/dairy/farm/controller/ReportController.java
+++ src/main/java/com/dairy/farm/controller/ReportController.java
@@ -2,25 +2,57 @@ package com.dairy.farm.controller;
 
 import com.dairy.farm.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayInputStream;
+import java.net.MalformedURLException;
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/api/reports")
 public class ReportController {
     @Autowired
     private ReportService reportService;
+    @GetMapping(value = "/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> downloadSalesPdf() throws MalformedURLException {
+        ByteArrayInputStream pdfStream = reportService.createSalesPdf();
 
-    @GetMapping("/excel")
-    public ResponseEntity<byte[]> generateExcelReport() {
-        byte[] excelReport = reportService.generateExcelReport();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=sales_report.pdf");
+
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=milk_sales_report.xlsx")
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(excelReport);
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(pdfStream));
+    }
+
+    @GetMapping("/sales/pdf" )
+    public ResponseEntity<InputStreamResource> getSalesPdf(
+            @RequestParam("startDate") String startDateStr,
+            @RequestParam("endDate") String endDateStr) throws MalformedURLException {
+
+        LocalDate startDate = LocalDate.parse(startDateStr);
+        LocalDate endDate = LocalDate.parse(endDateStr);
+
+        ByteArrayInputStream bais = reportService.createSalesBtwPdf(startDate, endDate);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=milk-report-" + LocalDate.now() + ".pdf");
+
+        return  ResponseEntity
+
+                .ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(bais));
+
     }
 }
